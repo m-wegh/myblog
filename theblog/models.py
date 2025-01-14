64518984f2d9b6a9a3fcf243a4d94e2fb8f9e59f
Matt W <matthewwegh@gmail.com>
2025-01-14 14:09:46 -0600

    Add dates. Remove edit/delete from home page

--- theblog/models.py
+++ theblog/models.py
@@ -1,14 +1,14 @@
 from django.db import models # type: ignore
-
-# Create your models here.
 from django.contrib.auth.models import User
 from django.urls import reverse
+from datetime import datetime, date
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
     title_tag = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
+    posting_date = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
@@ -16,3 +16,4 @@ class Post(models.Model):
     def get_absolute_url(self):
         #return reverse('article-detail', args=(str(self.id)))
         return reverse('home')
+    
