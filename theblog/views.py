99c9751a6a07ac15ff264a0bc7c750ee5b88c8e6
Matt W <matthewwegh@gmail.com>
2025-01-14 12:59:39 -0600

    Initial Commit

---
+++ theblog/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render # type: ignore
+from django.views.generic import ListView, DetailView, CreateView # type: ignore
+from .models import Post
+
+# Create your views here.
+#def home(request):
+#    return render(request, 'home.html', {})
+
+class HomeView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+
+class ArticleDetailView(DetailView):
+    model = Post
+    template_name = 'article_details.html'
+
+class AddPostView(CreateView):
+    model = Post 
+    template_name = 'add_post.html'
+    fields = '__all__'
+#    fields = ('title', 'body')
