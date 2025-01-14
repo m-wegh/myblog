59b4a97babb4227a9577c95a8fa40a48f13a9b7c
Matt W <matthewwegh@gmail.com>
2025-01-14 15:05:07 -0600

    Make Logout feature work

--- theblog/urls.py
+++ theblog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path # type: ignore
 from.views import AddPostView, ArticleDetailView, HomeView, UpdatePostView, DeletePostView
 urlpatterns = [
 #    path('', views.home,name="home"),
-    path('', HomeView.as_view(), name="home"),
+    path('', HomeView.as_view(), name='home'),
     path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'), 
     path('add_post/', AddPostView.as_view(), name='add_post'),
     path('article/<int:pk>/edit', UpdatePostView.as_view(), name='update_post'),
